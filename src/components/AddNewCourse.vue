180879412106541e149a05af2d9c59940160b744
saravanan2k03 <sarothehacker@gmail.com>
2023-09-07 22:32:20 +0530

    add

--- src/components/AddNewCourse.vue
+++ src/components/AddNewCourse.vue
@@ -37,19 +37,19 @@
                     <p class="text-3lg text-black font-semibold mt-2">Format</p>
                     <input type="text"
                         class="py-3 px-4 block w-full mt-4 border-2 border-gray-300 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                        placeholder="enter title">
+                        placeholder="enter format">
                 </div>
                 <div class="h-full min-w-[32%] flex flex-col  p-1">
                     <p class="text-3lg text-black font-semibold mt-2">Price</p>
                     <input type="text"
                         class="py-3 px-4 block w-full mt-4 border-2 border-gray-300 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                        placeholder="enter title">
+                        placeholder="enter price">
                 </div>
                 <div class="h-full min-w-[32%] flex flex-col  p-1">
                     <p class="text-3lg text-black font-semibold mt-2">Duration</p>
                     <input type="text"
                         class="py-3 px-4 block w-full mt-4 border-2 border-gray-300 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                        placeholder="enter title">
+                        placeholder="enter duration">
                 </div>
             </div>
         </div>
