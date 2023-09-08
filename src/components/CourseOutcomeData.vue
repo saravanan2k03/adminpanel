e099512ce696c9f0a1c7a6b08de657defcf9d3d9
saravanan2k03 <sarothehacker@gmail.com>
2023-09-08 18:57:22 +0530

    add

--- src/components/CourseOutcomeData.vue
+++ src/components/CourseOutcomeData.vue
@@ -1,5 +1,12 @@
 <template>
-    <p class="text-2xl text-black font-semibold mt-4">New Course</p>
+    <p class="text-3lg text-black font-semibold mt-4">Course Outcome Description</p>
+    <div class="min-h-[5%] max-h-[25%] min-w-[53%] max-w-[53%]  mt-3">
+        <input type="text"
+            class="py-3 px-4 block w-full  border-2 border-gray-300 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+            placeholder="enter short description" />
+    </div>
+    <p class="text-3lg text-black font-semibold mt-3">Outcomes</p>
+
 </template>
 
 
