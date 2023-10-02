ed9775827932859f7338be600f22b5d8f8345ece
saravanan2k03 <sarothehacker@gmail.com>
2023-10-02 09:59:53 +0530

    add

--- Api/config/db.js
+++ Api/config/db.js
@@ -6,6 +6,7 @@ const config ={
     accessKeyId:process.env.AWS_ACCESSKEYID,
     secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
     AWS_SDK_LOAD_CONFIG:1,
+    bucketName:process.env.AWS_BUCKET_NAME,
 };
 
 const DynamoDBConnection = (config) =>{
@@ -21,8 +22,8 @@ const DynamoDBConnection = (config) =>{
 
 const AwsS3Connection =(config)=>{
     try {
-        AWS.config.update(config);
-        const AwsS3Connection = new AWS.S3();
+
+        const AwsS3Connection = new AWS.S3(config);
         console.log('[INFO] connected To S3BUCKET');
         return AwsS3Connection;
     } catch (error) {
@@ -31,4 +32,5 @@ const AwsS3Connection =(config)=>{
 }
 const DynamoDB = DynamoDBConnection(config);
 const AWSS3 = AwsS3Connection(config);
+
 module.exports = {DynamoDB,AWSS3};
