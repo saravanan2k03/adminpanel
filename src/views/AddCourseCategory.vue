9d5ef7b24b6b219b3ee01886d248fd9278931ef3
saravanan2k03 <sarothehacker@gmail.com>
2023-10-05 06:37:49 +0530

    add checking file upload

--- src/views/AddCourseCategory.vue
+++ src/views/AddCourseCategory.vue
@@ -1,51 +1,58 @@
 <template>
-    
-<!-- Modal toggle -->
-<button data-modal-target="staticModal" data-modal-toggle="staticModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-  Toggle modal
-</button>
-
-<!-- Main modal -->
-<div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Static modal
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <div class="p-6 space-y-6">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
-            <!-- Modal footer -->
-            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="staticModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-hide="staticModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
-        </div>
+    <div class="widget">
+      <input type="file" ref="fileInput" @change="convertAndUpload" />
     </div>
-</div>
+  </template>
+  
+  <script>
+import axios from 'axios';
 
-</template>
+  export default {
+    name: 'AddCourseCategory',
+    methods: {
+      async convertAndUpload() {
+        const fileInput = this.$refs.fileInput;
+        const file = fileInput.files[0];
+  
+        if (file) {
+            const fileName = file.name; 
+            
 
+          const reader = new FileReader();
+  
+          reader.onload = async () => {
+            const base64String = reader.result.split(',')[1]; // Get the base64 string without the data URI prefix
+  
+            try {
+              const response = await this.uploadFile(base64String,fileName);
+  
+              // Handle the response from your Node.js API
+              console.log(response);
+            } catch (error) {
+              console.error(error);
+            }
+          };
+  
+          reader.readAsDataURL(file);
+        }
+      },
+  
+      async uploadFile(base64String,fileName) {
+        // Send the base64String to your Node.js API
+        const response = await axios.post('http://localhost:3000/admin//api/v1/courses', { SyllabusFiledata: base64String ,SyllabusFilename:fileName});
+        console.log(response.data)
+        return response.data;
 
-<script>
-export default {
-    name: "AddCourseCategory",
+      },
+    },
+  };
+  </script>
+  
 
-};
-</script>
+  <style>
+.widget{
+    margin-top: 300px;
+    margin-bottom: 300px;
+    margin-left: 300px;
+} 
+</style>
--- src/views/AddNewCourseCateView.vue
+++ src/views/AddNewCourseCateView.vue
@@ -6,7 +6,7 @@
             
             <Drawer CoursescolorClass="black" ProgramscolorClass="black" CourseCatecolorClass="blue"
                 ProgramCatecolorClass="black"></Drawer>
-
+            <AddCourseCategory></AddCourseCategory>
         </div>
 
     </div>
@@ -16,11 +16,13 @@
 <script>
 import Navbar from '../components/Navbar.vue'
 import Drawer from '../components/Drawer.vue'
+import AddCourseCategory from '../views/AddCourseCategory.vue'
 export default {
     name: 'AddNewCourseCategory',
     components: {
         Navbar,
         Drawer,
+        AddCourseCategory,
     }
 }
 </script>
